8d86f0828c4fbc01e47a869be03ca7f4ed7d2019
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-12 15:39:23 -0300

    abas

---
+++ Android - Aulas/Abas/app/src/main/java/com/aula/Fragment/EmAltaFragment.java
@@ -0,0 +1,27 @@
+package com.aula.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.aula.R;
+
+
+public class EmAltaFragment extends Fragment {
+
+    public EmAltaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_em_alta, container, false);
+    }
+
+}
