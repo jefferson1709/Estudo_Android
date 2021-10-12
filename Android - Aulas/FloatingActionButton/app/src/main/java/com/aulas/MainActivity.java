ce8660285733f70fa88b619246b6d66a64607c36
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-12 15:42:45 -0300

    FloatingActionButton

---
+++ Android - Aulas/FloatingActionButton/app/src/main/java/com/aulas/MainActivity.java
@@ -0,0 +1,29 @@
+package com.aulas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "FAB Pressionado", Snackbar.LENGTH_LONG)
+                        .setAction("Confirmar", null).show();
+
+            }
+        });
+    }
+}
